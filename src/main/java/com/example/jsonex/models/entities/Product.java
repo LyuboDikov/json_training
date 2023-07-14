9db33a36f0832652b65d497682ef5217712bdc7e
LyuboDikov <lyubo.dikov@icloud.com>
2023-07-14 15:54:35 +0300

    Initial commit
    (cherry picked from commit c86987bb5f6b5c17b402ec83cc3785548f1a40a8)

---
+++ src/main/java/com/example/jsonex/models/entities/Product.java
@@ -0,0 +1,56 @@
+package com.example.jsonex.models.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+public class Product extends BaseEntity {
+
+    private String name;
+    private BigDecimal price;
+    private User seller;
+    private User buyer;
+    public Product() {
+    }
+
+    @Column
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @ManyToOne
+    public User getSeller() {
+        return seller;
+    }
+
+    public void setSeller(User seller) {
+        this.seller = seller;
+    }
+
+    @ManyToOne
+    public User getBuyer() {
+        return buyer;
+    }
+
+    public void setBuyer(User buyer) {
+        this.buyer = buyer;
+    }
+}
